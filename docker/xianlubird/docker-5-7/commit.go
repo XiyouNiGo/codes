6886e4c0f7f89b9cc7c68c74e6e7af95babe65fb
XiyouNiGo <1275810355@qq.com>
2021-04-24 22:10:11 +0800

    ...

---
+++ docker/xianlubird/docker-5-7/commit.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/xianlubird/mydocker/container"
+	"os/exec"
+)
+
+func commitContainer(containerName, imageName string) {
+	mntURL := fmt.Sprintf(container.MntUrl, containerName)
+	mntURL += "/"
+
+	imageTar := container.RootUrl + "/" + imageName + ".tar"
+
+	if _, err := exec.Command("tar", "-czf", imageTar, "-C", mntURL, ".").CombinedOutput(); err != nil {
+		log.Errorf("Tar folder %s error %v", mntURL, err)
+	}
+}
