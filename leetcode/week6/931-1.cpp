30bf5499ecdc9a2f3d2a27006a894ec260c5f27d
XiyouNiGo <1275810355@qq.com>
2021-10-06 10:59:14 +0800

    leetcode 773

---
+++ leetcode/week6/931-1.cpp
@@ -0,0 +1,24 @@
+/*
+ * @Author: NiGo
+ * @LastEditors: NiGo
+ * @Date: 2021-10-05 21:17:59
+ * @LastEditTime: 2021-10-06 10:55:11
+ * @Description: file content
+ * @FilePath: /leetcode/week6/931-1.cpp
+ */
+#include <bits/stdc++.h>
+
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+ public:
+  int minFallingPathSum(vector<vector<int>> &matrix) {
+    int dp[matrix.size()][matrix[0].size()] = {0};
+
+    return 0;
+  }
+};
+
+int main(int argc, char *argv[]) { return 0; }
