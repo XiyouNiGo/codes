0582ab152657b972ef2d2d55a39187294c2d01bd
XiyouNiGo <1275810355@qq.com>
2021-10-06 22:05:20 +0800

    leetcode 172

--- leetcode/week6/136-1.cpp
+++ leetcode/week6/136-1.cpp
@@ -2,7 +2,7 @@
  * @Author: NiGo
  * @LastEditors: NiGo
  * @Date: 2021-10-06 21:18:19
- * @LastEditTime: 2021-10-06 21:18:19
+ * @LastEditTime: 2021-10-06 21:20:18
  * @Description: file content
  * @FilePath: /leetcode/week6/136-1.cpp
  */
@@ -11,12 +11,14 @@
 using namespace std;
 
 class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-
+ public:
+  int singleNumber(vector<int>& nums) {
+    int res = 0;
+    for (auto n : nums) {
+      res ^= n;
     }
+    return res;
+  }
 };
 
-int main(int argc, char *argv[]) {
-  return 0;
-}
+int main(int argc, char* argv[]) { return 0; }
---
+++ leetcode/week6/172-1.cpp
@@ -0,0 +1,22 @@
+/*
+ * @Author: NiGo
+ * @LastEditors: NiGo
+ * @Date: 2021-10-06 21:20:28
+ * @LastEditTime: 2021-10-06 21:20:29
+ * @Description: file content
+ * @FilePath: /leetcode/week6/172-1.cpp
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+
+    }
+};
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
