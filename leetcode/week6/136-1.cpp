54f2a1e50d84a9f330597075729ab66cb8524193
XiyouNiGo <1275810355@qq.com>
2021-10-06 21:20:14 +0800

    leetcode 136

---
+++ leetcode/week6/136-1.cpp
@@ -0,0 +1,22 @@
+/*
+ * @Author: NiGo
+ * @LastEditors: NiGo
+ * @Date: 2021-10-06 21:18:19
+ * @LastEditTime: 2021-10-06 21:18:19
+ * @Description: file content
+ * @FilePath: /leetcode/week6/136-1.cpp
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+    }
+};
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
