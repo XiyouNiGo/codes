0f7b79048c747359b38e0505a404616956dd2c44
XiyouNiGo <1275810355@qq.com>
2021-10-10 13:45:21 +0800

    leetcode 1140

--- leetcode/week6/1140-1.cpp
+++ leetcode/week6/1140-1.cpp
@@ -2,7 +2,7 @@
  * @Author: NiGo
  * @LastEditors: NiGo
  * @Date: 2021-10-08 11:42:26
- * @LastEditTime: 2021-10-08 18:14:35
+ * @LastEditTime: 2021-10-10 13:34:07
  * @Description: file content
  * @FilePath: /leetcode/week6/1140-1.cpp
  */
@@ -13,25 +13,22 @@ using namespace std;
 class Solution {
  public:
   int stoneGameII(vector<int>& piles) {
-    int n = piles.size();
-    vector<int> prefix(n + 1, 0);
-    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-
-    // dp[i][m]表示从位置i开始,M =
-    // m的情况下，第一位抓的玩家所能获取的最大数量石头
-
-    for (int i = 0; i < n; ++i) prefix[i + 1] = piles[i] + prefix[i];
-    for (int i = n - 1; i >= 0; --i) {
-      for (int m = 1; m <= n; ++m) {
-        int border = min(2 * m, n - i);
-        for (int j = 1; j <= border; ++j) {
-          dp[i][m] =
-              max(dp[i][m], prefix[n] - prefix[i] - dp[i + j][max(j, m)]);
+    int len = piles.size(), sum = 0;
+    vector<vector<int>> dp(len, vector<int>(len + 1));
+    for (int i = len - 1; i >= 0; i--) {
+      sum += piles[i];
+      for (int M = 1; M <= len; M++) {
+        if (i + 2 * M >= len) {
+          dp[i][M] = sum;
+        } else {
+          for (int x = 1; x <= 2 * M; x++) {
+            dp[i][M] = max(dp[i][M], sum - dp[i + x][max(M, x)]);
+          }
         }
       }
     }
-
     return dp[0][1];
   }
 };
+
 int main(int argc, char* argv[]) { return 0; }
