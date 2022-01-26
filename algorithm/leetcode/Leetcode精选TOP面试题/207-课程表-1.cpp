5b10a39cd9109606210a0cba6bbd5a01df020774
XiyouNiGo <1275810355@qq.com>
2022-01-26 23:37:01 +0800

    ..

--- algorithm/leetcode/Leetcode精选TOP面试题/207-课程表-1.cpp
+++ algorithm/leetcode/Leetcode精选TOP面试题/207-课程表-1.cpp
@@ -1,14 +1,42 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
 class Solution {
-public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+ private:
+  vector<vector<int>> edges;
+  vector<int> visited;
+  bool valid = true;
 
+ public:
+  void dfs(int u) {
+    visited[u] = 1;
+    for (int v : edges[u]) {
+      if (visited[v] == 0) {
+        dfs(v);
+        if (!valid) {
+          return;
+        }
+      } else if (visited[v] == 1) {
+        valid = false;
+        return;
+      }
     }
+    visited[u] = 2;
+  }
+
+  bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    edges.resize(numCourses);
+    visited.resize(numCourses);
+    for (const auto& info : prerequisites) {
+      edges[info[1]].push_back(info[0]);
+    }
+    for (int i = 0; i < numCourses && valid; ++i) {
+      if (!visited[i]) {
+        dfs(i);
+      }
+    }
+    return valid;
+  }
 };
 
-int main(int argc, char *argv[]) {
-  return 0;
-}
+int main(int argc, char* argv[]) { return 0; }
