e45a4f9ece4e67165b0febc3b94bc45502fd7980
XiyouNiGo <1275810355@qq.com>
2022-01-28 17:40:05 +0800

    ..

--- algorithm/leetcode/Leetcode精选TOP面试题/42-接雨水-2.cpp
+++ algorithm/leetcode/Leetcode精选TOP面试题/42-接雨水-2.cpp
@@ -4,9 +4,30 @@ using namespace std;
 
 class Solution {
  public:
-  
+  // 解法二： 动态规划
+  // O(N) O(N)
   int trap(vector<int>& height) {
+    int n = height.size();
+    if (n == 0) {
+      return 0;
+    }
+    vector<int> left_max(n);
+    left_max[0] = height[0];
+    for (int i = 1; i < n; ++i) {
+      left_max[i] = max(left_max[i - 1], height[i]);
+    }
 
+    vector<int> right_max(n);
+    right_max[n - 1] = height[n - 1];
+    for (int i = n - 2; i >= 0; --i) {
+      right_max[i] = max(right_max[i + 1], height[i]);
+    }
+
+    int res = 0;
+    for (int i = 0; i < n; ++i) {
+      res += min(left_max[i], right_max[i]) - height[i];
+    }
+    return res;
   }
 };
 
--- algorithm/leetcode/Leetcode精选TOP面试题/42-接雨水-3.cpp
+++ algorithm/leetcode/Leetcode精选TOP面试题/42-接雨水-3.cpp
@@ -1,12 +1,30 @@
 #include <bits/stdc++.h>
 
+#include <climits>
+
 using namespace std;
 
 class Solution {
  public:
-  
+  // 解法三： 双指针
+  // O(N) O(1)
   int trap(vector<int>& height) {
-
+    int res = 0;
+    int left = 0, right = height.size() - 1;
+    int left_max = INT_MIN, right_max = INT_MIN;
+    // 起码有两个凹槽才能计算
+    while (left < right) {
+      left_max = max(left_max, height[left]);
+      right_max = max(right_max, height[right]);
+      if (height[left] < height[right]) {
+        res += left_max - height[left];
+        left++;
+      } else {
+        res += right_max - height[right];
+        right--;
+      }
+    }
+    return res;
   }
 };
 
