ebba187c8590abafdadc0e5ba85100a43ae95ef5
XiyouNiGo <1275810355@qq.com>
2022-04-17 21:46:48 +0800

    learn

---
+++ go/mastering go/goFind.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+var minusD bool = false
+var minusF bool = false
+
+func walk(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+	mode := info.Mode()
+	if mode.IsRegular() && minusF {
+		fmt.Println("+", path)
+		return nil
+	}
+	if mode.IsDir() && minusD {
+		fmt.Println("*", path)
+		return nil
+	}
+	fmt.Println(path)
+	return nil
+}
+
+func main() {
+	starD := flag.Bool("d", false, "Signify directories")
+	plusF := flag.Bool("f", false, "Signify regular files")
+	flag.Parse()
+	flags := flag.Args()
+	Path := "."
+	if len(flags) == 1 {
+		Path = flags[0]
+	}
+	minusD = *starD
+	minusF = *plusF
+	err := filepath.Walk(Path, walk)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
