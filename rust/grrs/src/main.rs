78ab7aafb49f3a9ae77ab09d5eb055b3f1ac1764
XiyouNiGo <1275810355@qq.com>
2024-04-08 14:39:13 +0800

    grrs v2

--- rust/grrs/src/main.rs
+++ rust/grrs/src/main.rs
@@ -14,6 +14,13 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
-    // println!("{:?}, {:?}", args.path, args.pattern)
-    println!("{:?}", args)
+    println!("{:?}", args);
+
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
