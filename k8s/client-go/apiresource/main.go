868beb6bd009faab959d5d65a835ec4aeda05eca
linkai.666 <linkai.666@bytedance.com>
2025-03-21 14:44:26 +0800

    2025年 3月21日 星期五 14时44分26秒 CST

---
+++ k8s/client-go/apiresource/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	if err != nil {
+		log.Fatalf("Failed to get kubeconfig: %v", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("Failed to create clientset: %v", err)
+	}
+
+	grpList, err := clientset.Discovery().ServerGroups()
+	if err != nil {
+		log.Fatalf("Failed to get namespaced resource: %v", err)
+	}
+
+	for _, group := range grpList.Groups {
+		bytes, _ := json.Marshal(group)
+		fmt.Println(string(bytes))
+	}
+}
